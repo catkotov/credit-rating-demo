cf2ba0a3dd13fe53b7b15bb2539876a379326369
Александр Котов <cat.kotov@gmail.com>
2024-02-02 22:47:32 +0300

    Добавлена конфигурация теста

--- credit-rating-creation/src/test/java/org/cat/eye/credit/rating/CreditRatingRequestProcessingApplicationTest.java
+++ credit-rating-creation/src/test/java/org/cat/eye/credit/rating/CreditRatingRequestProcessingApplicationTest.java
@@ -2,10 +2,13 @@ package org.cat.eye.credit.rating;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.main.allow-bean-definition-overriding=true"},
+classes = {TestCreditRatingRequestProcessingApplication.class})
+//@Import({TestCreditRatingRequestProcessingApplication.class})
 class CreditRatingRequestProcessingApplicationTest {
 
     @Test
